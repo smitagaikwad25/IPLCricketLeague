848e296734908f57e36cadc33eea9616bbbf02e1
smitagaikwad25 <gaikwadsmit021@gmail.com>
2020-01-20 18:37:56 +0530

    Refactor: remove commited code for analyser class

--- src/main/java/com/bridgelabz/CricketLeagueAnalyser.java
+++ src/main/java/com/bridgelabz/CricketLeagueAnalyser.java
@@ -22,31 +22,4 @@ public class CricketLeagueAnalyser {
         System.out.println(sortedData);
         return sortedData;
     }
-
-//    public List<BatsmanDetails> sortingToGetTopBattingAvgOfCricketers() throws CricketLeagueException {
-//        if (batsmanDetailsArrayList == null || batsmanDetailsArrayList.size() == 0) {
-//            throw new CricketLeagueException("No Data Available", CricketLeagueException.ExceptionType.NO_DATA_AVAILABLE);
-//        }
-//        List topBattingAvg = batsmanDetailsArrayList.stream().sorted(Comparator.comparing(BatsmanDetails::getAvg).reversed()).collect(Collectors.toList());
-//        return topBattingAvg;
-//    }
-
-//    public List<BatsmanDetails> sortingToKnowTopStrikingRatesOfTheBatsman() throws CricketLeagueException {
-//        if (batsmanDetailsArrayList == null || batsmanDetailsArrayList.size() == 0) {
-//            throw new CricketLeagueException("No Data Available", CricketLeagueException.ExceptionType.NO_DATA_AVAILABLE);
-//        }
-//        List topStrikingRates = batsmanDetailsArrayList.stream().sorted(Comparator.comparing(BatsmanDetails::getSr).reversed()).collect(Collectors.toList());
-//        return topStrikingRates;
-//    }
-
-//    public List<BatsmanDetails> sortingAccordingNumberOfFourAndNumberOfSix() throws CricketLeagueException {
-//        if (batsmanDetailsArrayList == null || batsmanDetailsArrayList.size() == 0) {
-//            throw new CricketLeagueException("No Data Available", CricketLeagueException.ExceptionType.NO_DATA_AVAILABLE);
-//        }
-//
-//        Comparator<BatsmanDetails> sortNumberOfFourAndSix = (obj1, obj2) -> ((obj1.four * 4 + obj1.six * 6) < (obj2.four * 4 + obj2.six * 6) ? 1 : -1);
-//        Collections.sort(batsmanDetailsArrayList, sortNumberOfFourAndSix);
-//        return batsmanDetailsArrayList;
-//    }
-
 }
